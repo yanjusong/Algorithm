d4936ddd5bf844df9997c928ddefe4405105e407
yanjusong <1023651409@qq.com>
2017-05-09 15:10:27 +0800

    修改readme

--- 排序算法/堆排序.cpp
+++ 排序算法/堆排序.cpp
@@ -8,10 +8,10 @@ void print(int *, int);
 int main(int argc, char *argv[]) 
 {
     int nums[] = {16, 7, 3, 20, 17, 8};
-	if( heapSort(nums, sizeof(nums)/sizeof(int)) ) {
-		print(nums, 6);
-	}
-	return 0;
+    if( heapSort(nums, sizeof(nums)/sizeof(int)) ) {
+        print(nums, 6);
+    }
+    return 0;
 }
 
 int heapSort(int *nums, int length) 
@@ -26,63 +26,63 @@ int heapSort(int *nums, int length)
     
     // 建堆
     for(int i = length/2 - 1; i >= 0; --i) {
-		heapAdjust(nums, i, length);
-	}
-	
-	// 堆头堆尾交换 + 调整堆
-	for(int i = 0; i < length - 1; ++i) {
-		// 交换
-		swap(&nums[0], &nums[length - i - 1]);
-		// 调整
-		heapAdjust(nums, 0, length - i - 1);
-	}
-	
-	return 1;
+        heapAdjust(nums, i, length);
+    }
+    
+    // 堆头堆尾交换 + 调整堆
+    for(int i = 0; i < length - 1; ++i) {
+        // 交换
+        swap(&nums[0], &nums[length - i - 1]);
+        // 调整
+        heapAdjust(nums, 0, length - i - 1);
+    }
+    
+    return 1;
 }
 
 int heapAdjust(int *nums, int index, int length)
 {
-	int tmp = nums[index];
-	
-	int i = index;
-	int j = i;
-	
-	while(i < length) {
-		int bigOne = i;
-		if(i*2 + 1 < length && tmp < nums[i*2 + 1]) {
-			bigOne = i*2 + 1;
-		}
-		if(i*2 + 2 < length && tmp < nums[i*2 + 2]) {
-			if(bigOne == i) {
-				bigOne = i*2 + 2;
-			} else if(nums[i*2 + 2] > nums[i*2 + 1]) {
-				bigOne = i*2 + 2;
-			}
-		}
-		
-		if(bigOne == i) {
-			break;
-		}
-		
-		nums[i] = nums[bigOne];
-		j = i;
-		i = bigOne;
-	}
-	nums[i] = tmp;
-	
-	return 1;
+    int tmp = nums[index];
+    
+    int i = index;
+    int j = i;
+    
+    while(i < length) {
+        int bigOne = i;
+        if(i*2 + 1 < length && tmp < nums[i*2 + 1]) {
+            bigOne = i*2 + 1;
+        }
+        if(i*2 + 2 < length && tmp < nums[i*2 + 2]) {
+            if(bigOne == i) {
+                bigOne = i*2 + 2;
+            } else if(nums[i*2 + 2] > nums[i*2 + 1]) {
+                bigOne = i*2 + 2;
+            }
+        }
+        
+        if(bigOne == i) {
+            break;
+        }
+        
+        nums[i] = nums[bigOne];
+        j = i;
+        i = bigOne;
+    }
+    nums[i] = tmp;
+    
+    return 1;
 }
 
 void swap(int *x, int *y)
 {
     int tmp = *x;
-	*x = *y;
-	*y = tmp;
+    *x = *y;
+    *y = tmp;
 }
 
 void print(int *nums, int length) 
 {
-	for(int i = 0; i < length; ++i)
-		printf("%d ", nums[i]);
-	printf("\n");
+    for(int i = 0; i < length; ++i)
+        printf("%d ", nums[i]);
+    printf("\n");
 }
--- 查找算法/二分查找.cpp
+++ 查找算法/二分查找.cpp
@@ -5,43 +5,43 @@
 
 int binarySearch(int *nums, int length, int key)
 {
-	if(!nums || length <= 0) {
-		return ERROR;
-	}
-	
-	int low = 0;
-	int high = length - 1;
-	int mid;
-	while(low <= high) {
-		mid = (low + high)/2;
-		if(nums[mid] == key) {
-			return mid;
-		} else if(nums[mid] > key) {
-			high = mid - 1;
-		} else {
-			low = mid + 1;
-		}
-	}
-	
-	return FAIL;
+    if(!nums || length <= 0) {
+        return ERROR;
+    }
+    
+    int low = 0;
+    int high = length - 1;
+    int mid;
+    while(low <= high) {
+        mid = (low + high)/2;
+        if(nums[mid] == key) {
+            return mid;
+        } else if(nums[mid] > key) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    
+    return FAIL;
 }
 
 int main(int argc, char *argv[])
 {
-	int nums[] = {1,2,3,4,5,6,7,8,9,10};
-	
-	printf( "%d \n", binarySearch(nums, sizeof(nums)/sizeof(int), 1) );
-	printf( "%d \n", binarySearch(nums, sizeof(nums)/sizeof(int), 2) );
-	printf( "%d \n", binarySearch(nums, sizeof(nums)/sizeof(int), 3) );
-	printf( "%d \n", binarySearch(nums, sizeof(nums)/sizeof(int), 4) );
-	printf( "%d \n", binarySearch(nums, sizeof(nums)/sizeof(int), 5) );
-	printf( "%d \n", binarySearch(nums, sizeof(nums)/sizeof(int), 6) );
-	printf( "%d \n", binarySearch(nums, sizeof(nums)/sizeof(int), 7) );
-	printf( "%d \n", binarySearch(nums, sizeof(nums)/sizeof(int), 8) );
-	printf( "%d \n", binarySearch(nums, sizeof(nums)/sizeof(int), 9) );
-	printf( "%d \n", binarySearch(nums, sizeof(nums)/sizeof(int), 10) );
-	printf( "%d \n", binarySearch(nums, sizeof(nums)/sizeof(int), 11) );
-	printf( "%d \n", binarySearch(nums, sizeof(nums)/sizeof(int), 0) );
-	printf( "%d \n", binarySearch(nums, -3, 0) );
-	return 0;
+    int nums[] = {1,2,3,4,5,6,7,8,9,10};
+    
+    printf( "%d \n", binarySearch(nums, sizeof(nums)/sizeof(int), 1) );
+    printf( "%d \n", binarySearch(nums, sizeof(nums)/sizeof(int), 2) );
+    printf( "%d \n", binarySearch(nums, sizeof(nums)/sizeof(int), 3) );
+    printf( "%d \n", binarySearch(nums, sizeof(nums)/sizeof(int), 4) );
+    printf( "%d \n", binarySearch(nums, sizeof(nums)/sizeof(int), 5) );
+    printf( "%d \n", binarySearch(nums, sizeof(nums)/sizeof(int), 6) );
+    printf( "%d \n", binarySearch(nums, sizeof(nums)/sizeof(int), 7) );
+    printf( "%d \n", binarySearch(nums, sizeof(nums)/sizeof(int), 8) );
+    printf( "%d \n", binarySearch(nums, sizeof(nums)/sizeof(int), 9) );
+    printf( "%d \n", binarySearch(nums, sizeof(nums)/sizeof(int), 10) );
+    printf( "%d \n", binarySearch(nums, sizeof(nums)/sizeof(int), 11) );
+    printf( "%d \n", binarySearch(nums, sizeof(nums)/sizeof(int), 0) );
+    printf( "%d \n", binarySearch(nums, -3, 0) );
+    return 0;
 }
